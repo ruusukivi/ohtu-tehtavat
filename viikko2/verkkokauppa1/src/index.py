a0aeaa5903370bb6c34f4402e430d9502f7605df
ruusukivi <ruusukivi@gmail.com>
2022-01-23 13:36:28 +0200

    vko2, tehtava11

--- viikko2/verkkokauppa1/src/index.py
+++ viikko2/verkkokauppa1/src/index.py
@@ -6,12 +6,12 @@ from kirjanpito import Kirjanpito
 
 
 def main():
-    kauppa = Kauppa(
-        Varasto.get_instance(),
-        Pankki.get_instance(),
-        Viitegeneraattori.get_instance()
-    )
-
+    viitegeneraattori = Viitegeneraattori()
+    kirjanpito = Kirjanpito()
+    varasto = Varasto(kirjanpito)
+    pankki = Pankki(kirjanpito)
+    kauppa = Kauppa(varasto, pankki, viitegeneraattori)
+    
     # kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
     kauppa.aloita_asiointi()
     kauppa.lisaa_koriin(1)
@@ -19,19 +19,18 @@ def main():
     kauppa.lisaa_koriin(3)
     kauppa.poista_korista(1)
     kauppa.tilimaksu("Pekka Mikkola", "1234-12345")
-
+    
     # seuraava asiakas
     kauppa.aloita_asiointi()
-
+    
     for _ in range(0, 24):
         kauppa.lisaa_koriin(5)
 
     kauppa.tilimaksu("Arto Vihavainen", "3425-1652")
 
     # kirjanpito
-    for tapahtuma in Kirjanpito.get_instance().tapahtumat:
+    for tapahtuma in kirjanpito.tapahtumat:
         print(tapahtuma)
-
 
 if __name__ == "__main__":
     main()
