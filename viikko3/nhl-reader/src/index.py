9c6393a737731f7183eb4f3cf8c210b87a73ef8d
ruusukivi <ruusukivi@gmail.com>
2022-01-29 19:05:07 +0200

    vko3, tehtava 1

---
+++ viikko3/nhl-reader/src/index.py
@@ -0,0 +1,30 @@
+import requests
+import datetime
+from player import Player
+
+def main():
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        if player_dict['nationality'] == "FIN":
+            player = Player(
+                player_dict['name'],
+                player_dict['team'],
+                player_dict['goals'],
+                player_dict['assists']
+            )
+            players.append(player)
+
+    print(f"Players from FIN {datetime.datetime.now()}")
+
+    for player in players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
---
+++ viikko3/nhl-reader/src/player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self, name, team, goals, assists):
+        self.name = name
+        self.team = team
+        self.goals = goals
+        self.assists = assists
+    
+    def __str__(self):
+        player = f"{self.name} team {self.team} goals {self.goals} assists {self.assists}"
+        return player
