d2e2cd0569168c6a3f372a28c19897079f275427
ruusukivi <ruusukivi@gmail.com>
2022-02-12 19:53:27 +0200

    vko 4, tehtava 5, 3: ostoskorin hinta vastaa siinä olevan tavaran hintaa

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -16,7 +16,10 @@ class Ostoskori:
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        return 0
+        hinta = 0
+        for ostos in self.ostokset:
+            hinta += ostos.hinta()
+        return hinta
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
