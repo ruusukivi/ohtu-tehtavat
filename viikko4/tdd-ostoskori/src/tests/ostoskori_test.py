3b25d88aa6542631cfe065a172b41df0b444a949
ruusukivi <ruusukivi@gmail.com>
2022-02-12 22:13:18 +0200

    vko 4, tehtava 5, 13: kahden saman tuotteen lisayksen ja yhden poiston jalkeen maara oikein

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -90,4 +90,11 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(ostokset[0].tuotteen_nimi(), "Maito")
         self.assertEqual(ostokset[0].lukumaara(), 2)
 
+    def test_kahden_saman_tuotteen_lisaamisen_ja_yhden_poiston_jalkeen_maara_oikein(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(ostokset[0].lukumaara(), 1)
 
