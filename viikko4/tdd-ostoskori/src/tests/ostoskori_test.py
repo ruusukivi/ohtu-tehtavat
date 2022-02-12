73f8ac0ed791c74d6483c6aa50da6676b39631b0
ruusukivi <ruusukivi@gmail.com>
2022-02-12 20:00:01 +0200

    vko 4, tehtava 5, 6: korissa kaksi samaa tuotetta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -33,3 +33,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(murot)
         self.assertEqual(self.kori.hinta(), 8)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        maito2 = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito2)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
