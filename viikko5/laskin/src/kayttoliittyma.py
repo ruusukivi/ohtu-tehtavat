99cda3c31a1fef189008a89ae6d7594bd415d03c
ruusukivi <ruusukivi@gmail.com>
2022-04-10 21:18:40 +0300

    vko5, tehtava5

--- viikko5/laskin/src/kayttoliittyma.py
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -1,13 +1,14 @@
+from curses.ascii import isblank
 from enum import Enum
 from tkinter import ttk, constants, StringVar
 
+edelliset = []
 
 class Komento(Enum):
     SUMMA = 1
     EROTUS = 2
     NOLLAUS = 3
     KUMOA = 4
-
 
 class Kayttoliittyma:
     def __init__(self, sovellus, root):
@@ -63,11 +64,15 @@ class Kayttoliittyma:
         self._kumoa_painike.grid(row=2, column=3)
 
     def _lue_syote(self):
-        return self._syote_kentta.get()
+        syote = self._syote_kentta.get()
+        if syote == "":
+            syote = 0
+        return syote
 
     def _suorita_komento(self, komento):
         komento_olio = self._komennot[komento]
-        komento_olio.suorita()
+        if komento_olio:
+           komento_olio.suorita()
         self._kumoa_painike["state"] = constants.NORMAL
 
         if self._sovellus.tulos == 0:
@@ -85,8 +90,8 @@ class Summa:
         self.sovellus = sovellus
 
     def suorita(self):
+        edelliset.append(self.sovellus.tulos)
         return self.sovellus.plus(int(self.syote()))
-
 
 class Erotus:
     def __init__(self, sovellus, syote):
@@ -94,14 +99,15 @@ class Erotus:
         self.sovellus = sovellus
 
     def suorita(self):
+        edelliset.append(self.sovellus.tulos)
         return self.sovellus.miinus(int(self.syote()))
-
 
 class Nollaus:
     def __init__(self, sovellus, syote):
         self.syote = syote
         self.sovellus = sovellus
     def suorita(self):
+        edelliset.append(self.sovellus.tulos)
         return self.sovellus.nollaa()
 
 class Kumoa:
@@ -110,4 +116,7 @@ class Kumoa:
         self.sovellus = sovellus
 
     def suorita(self):
-        pass
+        if len(edelliset) > 0:
+            self.sovellus.aseta_arvo(edelliset.pop())
+        else:
+            self.sovellus.aseta_arvo(0)
